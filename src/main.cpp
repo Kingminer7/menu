7cfdf946882966dc4612f1593bce6505130df652
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-11-26 20:28:42 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
 #include <Geode/Geode.hpp>
 
-#include <imgui-cocos.hpp>
-
-$on_mod(Loaded) {
-    ImGuiCocos::get().setup([] {
-	        // this runs after imgui has been setup,
-			        // its a callback as imgui will be re initialized when toggling fullscreen,
-					        // so use this to setup any themes and or fonts!
-							    }).draw([] {
-								        ImGui::Begin("My awesome window");
-
-										        ImGui::Button("Awesome button");
-												        
-														        ImGui::End();
-																    });
-																	}
-
 using namespace geode::prelude;
 
 #include <Geode/modify/MenuLayer.hpp>
