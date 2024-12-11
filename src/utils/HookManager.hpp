375112d48ee6d0286cdfce3d6bb7de84bdbdacda
km7dev <gavin@t8er.com>
2024-12-11 09:53:49 -0500

    if this works then my laptop is not happy with imgui for some reason

--- src/utils/HookManager.hpp
+++
@@ -1,7 +0,0 @@
-using namespace geode::prelude;
-
-namespace summit::HookManager {
-  bool disableHook(std::string id);
-  bool enableHook(std::string id);
-  bool registerHook(std::string id, Hook* hook);
-} // namespace summit
