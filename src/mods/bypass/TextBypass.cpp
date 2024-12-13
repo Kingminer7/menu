11bb6463f7e518111818cf6845e9dde12a3dcac7
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2024-12-13 16:54:42 -0500

    oops

--- src/mods/bypass/TextBypass.cpp
+++ src/mods/bypass/TextBypass.cpp
@@ -45,6 +45,14 @@ std::string TextBypassMod::getTab() const {
     return "Bypass";
 }
 
+void TextBypassMod::onLengthBypass(bool toggled) {
+    summit::Config::setValue<bool>("bypass.textlengthbypass.enabled", toggled);
+}
+
+void TextBypassMod::onCharBypass(bool toggled) {
+    summit::Config::setValue<bool>("bypass.textcharbypass.enabled", toggled);
+}
+
 #include <Geode/modify/CCTextInputNode.hpp>
 class $modify (CCTextInputNode) {
     struct Fields {
@@ -52,14 +60,14 @@ class $modify (CCTextInputNode) {
         std::string m_origChars = "";
     };
     void updateLabel(std::string p0) {
-        if (summit::Config::getValue<bool>("bypass.textlengthbypass.enabled", false);) {
+        if (summit::Config::getValue<bool>("bypass.textlengthbypass.enabled", false)) {
             if (m_maxLabelLength != INT_MAX) {
                 m_fields->m_origLength = m_maxLabelLength;
                 setMaxLabelLength(INT_MAX);
             }
         } else if (m_maxLabelLength == INT_MAX) setMaxLabelLength(m_fields->m_origLength);
 
-        if (summit::Config::getValue<bool>("bypass.textcharbypass.enabled", false);) {
+        if (summit::Config::getValue<bool>("bypass.textcharbypass.enabled", false)) {
             if (m_allowedChars != "`1234567890-=qwertyuiop[]\\asdfghjkl;'cxzvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? ") {
                 m_fields->m_origChars = m_allowedChars;
                 setAllowedChars("`1234567890-=qwertyuiop[]\\asdfghjkl;'cxzvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>? ");
