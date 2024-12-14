f0d1ac2c3974e1df61defd4a45a2786c0f6ff7b5
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-12-14 18:45:09 -0500

    Update ImGuiMain.hpp

--- src/ui/imgui/ImGuiMain.hpp
+++ src/ui/imgui/ImGuiMain.hpp
@@ -12,5 +12,7 @@ namespace summit::ui::imgui {
     std::vector<std::string> getFontStyles(std::string font);
     std::map<std::pair<std::string, std::string>, ImFont*> getImFonts();
     bool setFont(std::string font, std::string style);
+    bool pushFont(std::string font, std::string style);
+    void popFont();
     bool isFontStyleSupported(std::string font, std::string style);
 }
