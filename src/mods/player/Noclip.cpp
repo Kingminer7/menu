47f82763380eb1bde66164deee622ba14db60f38
km7dev <gavin@t8er.com>
2024-12-14 20:15:59 -0500

    fix a default thing + add player noclips

--- src/mods/player/Noclip.cpp
+++ src/mods/player/Noclip.cpp
@@ -3,9 +3,17 @@
 #include "Noclip.hpp"
 
 void NoclipMod::init() {
-    summit::Config::setValueIfUnset<bool>("player.noclip.enabled", true);
-    toggled = summit::Config::getValue<bool>("player.noclip.enabled", true);
-    lastToggled = toggled;
+    summit::Config::setValueIfUnset<bool>("player.noclip.enabled", false);
+    noclip = summit::Config::getValue<bool>("player.noclip.enabled", false);
+    lastNoclip = noclip;
+
+    summit::Config::setValueIfUnset<bool>("player.noclip.player1", true);
+    player1 = summit::Config::getValue<bool>("player.noclip.player1", true);
+    lastPlayer1 = player1;
+
+    summit::Config::setValueIfUnset<bool>("player.noclip.player2", true);
+    player2 = summit::Config::getValue<bool>("player.noclip.player2", true);
+    lastPlayer2 = player2;
 }
 
 void NoclipMod::update(float dt) {
@@ -13,17 +21,40 @@ void NoclipMod::update(float dt) {
 }
 
 void NoclipMod::renderImGui() {
-    ImGui::Checkbox("Noclip", &toggled);
+    ImGui::Checkbox("Noclip", &noclip);
     if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
     {
         ImGui::SetTooltip("Prevents you from dying.");
     }
     ImGui::SameLine(ImGui::GetWindowWidth() - 30);
     ImGui::SetNextItemWidth(50);
-    ImGui::Button(">");
-    if (lastToggled != toggled) {
-        lastToggled = toggled;
-        onToggle(toggled);
+    if (ImGui::Button(">")) ImGui::OpenPopup("Noclip");
+        if (ImGui::BeginPopup("Noclip")) {
+        ImGui::Checkbox("Player 1", &player1);
+        if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
+        {
+            ImGui::SetTooltip("Prevents player 1 from dying.");
+        }
+        if (lastPlayer1 != player1) {
+            lastPlayer1 = player1;
+            summit::Config::setValue<bool>("player.noclip.player1", player1);
+        }
+            
+        ImGui::Checkbox("Player 2", &player2);
+        if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
+        {
+            ImGui::SetTooltip("Prevents player 2 from dying.");
+        }
+        if (lastPlayer2 != player2) {
+            lastPlayer2 = player2;
+            summit::Config::setValue<bool>("player.noclip.player2", player2);
+        }
+
+        ImGui::EndPopup();
+    }
+    if (lastNoclip != noclip) {
+        lastNoclip = noclip;
+        onNoclip(noclip);
     }
 }
 
@@ -35,13 +66,28 @@ std::string NoclipMod::getTab() const {
     return "Player";
 }
 
-void NoclipMod::onToggle(bool toggled) {
+void NoclipMod::onNoclip(bool toggled) {
     summit::Config::setValue<bool>("player.noclip.enabled", toggled);
+}
+
+void NoclipMod::onPlayer1(bool toggled) {
+    summit::Config::setValue<bool>("player.noclip.player1", toggled);
+}
+
+void NoclipMod::onPlayer2(bool toggled) {
+    summit::Config::setValue<bool>("player.noclip.player2", toggled);
 }
 
 #include <Geode/modify/PlayLayer.hpp>
 class $modify (PlayLayer) {
     void destroyPlayer(PlayerObject *player, GameObject *obj) {
-        if (!summit::Config::getValue<bool>("player.noclip.enabled", false) || obj == m_anticheatSpike) PlayLayer::destroyPlayer(player, obj);
+        if (!summit::Config::getValue<bool>("player.noclip.enabled", false) || obj == m_anticheatSpike) {
+            PlayLayer::destroyPlayer(player, obj);
+            return;
+        }
+        if ((m_player1 == player && !summit::Config::getValue<bool>("player.noclip.player1", false)) || (m_player2 == player && !summit::Config::getValue<bool>("player.noclip.player2", false))) {
+            PlayLayer::destroyPlayer(player, obj);
+            return;
+        }
     }
 };
--- src/mods/player/Noclip.hpp
+++ src/mods/player/Noclip.hpp
@@ -4,15 +4,23 @@
 
 class NoclipMod : public summit::mods::Mod {
     private:
-        bool toggled = false;
-        bool lastToggled = false;
+        bool noclip = false;
+        bool lastNoclip = false;
+
+        bool player1 = true;
+        bool lastPlayer1 = true;
+
+        bool player2 = true;
+        bool lastPlayer2 = true;
     public:
         void init() override;
         void update(float dt) override;
         void renderImGui() override;
         std::string getId() const override;
         std::string getTab() const override;
-        void onToggle(bool toggled);
+        void onNoclip(bool toggled);
+        void onPlayer1(bool toggled);
+        void onPlayer2(bool toggled);
 };
 
 REGISTER_MOD(new NoclipMod());
