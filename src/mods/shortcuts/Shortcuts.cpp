b13e8e4c13b12a46c73cd67c56ffebf8fb7e01cb
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-12-18 07:41:52 -0500

    if this doesnt work...

---
+++ src/mods/shortcuts/Shortcuts.cpp
@@ -0,0 +1,29 @@
+#include <imgui-cocos.hpp>
+#include "../../Summit.hpp"
+#include "Shortcuts.hpp"
+
+void ShortcutsMod::init() {
+    
+}
+
+void ShortcutsMod::update(float dt) {
+    
+}
+
+void ShortcutsMod::renderImGui() {
+    if (ImGui::Button("Safe Mode")) {
+        m_settingsCall(nullptr);
+    }
+    if (ImGui::IsItemHovered(ImGuiHoveredFlags_DelayShort))
+    {
+        ImGui::SetTooltip("Opens Settings");
+    }
+}
+
+std::string ShortcutsMod::getId() const {
+    return "shortcuts";
+}
+
+std::string ShortcutsMod::getTab() const {
+    return "Shortcuts";
+}
