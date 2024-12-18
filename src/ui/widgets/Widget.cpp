1f51f0738e33559fce0090819a5f1efd8ef0b3b1
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2024-12-18 09:47:23 -0500

    start widgets

--- src/ui/widgets/Widget.cpp
+++ src/ui/widgets/Widget.cpp
@@ -1,1 +1,42 @@
 #include "Widget.hpp"
+
+namespace summit::ui::widgets {
+  Widget *Widget::addToggle(std::string id, std::function<void(bool toggled)> callback, bool default_) {
+    m_components.push_back(new Toggle{id, default_, callback});
+    return this;
+  }
+
+  Widget *Widget::addButton(std::string id, std::function<void()> callback) {
+    m_components.push_back(new Button{id, callback});
+    return this;
+  }
+
+  Widget *Widget::remove(std::string id) {
+    for (auto it = m_components.begin(); it != m_components.end(); it++) {
+      if ((*it)->id == id) {
+        m_components.erase(it);
+        delete *it;
+        break;
+      }
+    }
+    return this;
+  }
+
+  Widget *Widget::setLabel(std::string label) {
+    m_label = label;
+    return this;
+  }
+
+  Widget *Widget::setDescription(std::string desc) {
+    m_description = desc;
+    return this;
+  }
+
+  Widget *Widget::registerOption() {
+    return this;
+  }
+
+  Widget *Widget::unregisterOption() {
+    return this;
+  }
+}
--- src/ui/widgets/Widget.hpp
+++ src/ui/widgets/Widget.hpp
@@ -1,9 +1,45 @@
 namespace summit::ui::widgets {
+
+  struct Component {
+    std::string id;
+  };
+  struct Toggle : Component {
+    bool toggled = false;
+    std::function<void(bool toggled)> callback;
+  };
+  struct Button : Component {
+    std::function<void()> callback;
+  };
+
   class Widget {
     protected:
-      std::map<std::string, std::function<void(bool toggled)>> callbacks;
+      std::string m_id;
+      std::string m_label;
+      std::string m_description;
+      std::vector<Component*> m_components;
+      Widget() {}
+      void init() {}
     public:
       // these all return the widget so you can call things like myWidget->addThing()->setThat() etc
-      Widget *addToggle(bool default, std::string id, std::function<void(bool toggled)> callback);
+      // @brief Adds a toggle to the widget
+      Widget *addToggle(std::string id, std::function<void(bool toggled)> callback, bool default_);
+      // @brief Adds a button to the widget
+      Widget *addButton(std::string id, std::function<void()> callback);
+
+      Widget *remove(std::string id);
+
+      // @brief Sets the label of the widget
+      Widget *setLabel(std::string label);
+      // @brief Sets the description of the widget
+      Widget *setDescription(std::string desc);
+      
+      // @brief Register this option with the UI
+      Widget *registerOption();
+      // @brief Unregister this option with the UI
+      Widget *unregisterOption();
+
+      static Widget *create(std::string id) {
+        return new Widget();
+      }
   };
 }
