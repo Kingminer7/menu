8943e8427db4f15d3196f07a1aacef491c662f5f
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2024-12-18 07:41:21 -0500

    Update Shortcuts.hpp

--- src/mods/shortcuts/Shortcuts.hpp
+++ src/mods/shortcuts/Shortcuts.hpp
@@ -3,6 +3,8 @@
 #include "../Mods.hpp"
 
 class ShortcutsMod : public summit::mods::Mod {
+    protected:
+        cocos2d::SEL_MenuHandler m_settingsCall;
     public:
         void init() override;
         void update(float dt) override;
